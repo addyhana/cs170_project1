563e65a39dae4003d99cee2beb3424822de5ec2a
anishasiva <anishasiva21@gmail.com>
2024-04-29 23:12:36 -0700

    created menu page

--- CS170_Project/main.py
+++ CS170_Project/main.py
@@ -2,11 +2,17 @@ import os
 import sys
 
 from src.state import State
+from src.menu import main_menu
 
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd()) #you can access files in the same directory this way
 
-state = State()
-board = state.build_board()
-state.print_board(board=board)
-print()
-state.print_board(state.end)
+
+
+#state = State()
+#board = state.build_board()
+#state.print_board(board=board)
+#print()
+#state.print_board(board=state.end)
+
+main_menu()
+
