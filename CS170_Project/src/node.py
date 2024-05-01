6183859699515b13f0d0d6851dd2050249a2087f
anishasiva <anishasiva21@gmail.com>
2024-04-30 19:21:15 -0700

    adding node class

--- CS170_Project/src/node.py
+++ CS170_Project/src/node.py
@@ -1,12 +1,24 @@
+import numpy as np
+
 class Node:
     def __init__(self,
-                 value: int,
-                 next: None) -> None:
+                 state,
+                 g_value, #be careful about type here
+                 h_value,
+                 parent) -> None:
         """_summary_
 
         Args:
-            value (int): the number
-            next (None): next node 
+            state (np.array): the state of the 8 puzzle game
+            g_value: depth of node on the tree
+            h_value: heuristic 
+            
         """
-        self.value = value 
-        self.next = next
+        self.state = state
+        self.g_value = g_value
+        self.h_value = h_value
+        self.f_value = g_value + h_value #or g_value + f_value but then dont make it a parameter? 
+        self.parent = None
+        self.next = None 
+        
+        
