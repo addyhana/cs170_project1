6f3040c6afafc4d4d2116a7912a040123838e1ca
PajakaL <60051149+PajakaL@users.noreply.github.com>
2024-05-04 13:32:04 -0700

    Update search_functions.py 2
    didn't push properly the first time

--- CS170_Project/src/search_functions.py
+++ CS170_Project/src/search_functions.py
@@ -58,3 +58,47 @@ class Algorithms:
                             frontier.put((depth + order_determiner, move))
 
         return None
+
+
+def euclidean_distance(state, goal_state):
+    # Calculate Euclidean distance between current state and goal state
+    return math.sqrt(np.sum((state - goal_state) ** 2))
+
+def euclidean(initial_state, goal_state):
+    state = State(initial_state, goal_state)
+    frontier = PriorityQueue()
+    explored = set()
+    path = []
+    depth = 0
+
+    frontier.put((0, initial_state))  # Priority queue sorted by f(n) = g(n) + h(n)
+    num_nodes = 1  # start at 1 to account for initial state
+
+    while not frontier.empty():
+        _, curr_state = frontier.get()
+        path.append(curr_state)
+
+        if np.array_equal(curr_state, goal_state):
+            return path
+
+        explored.add(tuple(curr_state))
+        state.set_state(curr_state)
+
+        up_move = state.move_up()
+        down_move = state.move_down()
+        left_move = state.move_left()
+        right_move = state.move_right()
+        moves = [up_move, down_move, left_move, right_move]
+
+        num_nodes += 1
+        depth = np.floor(np.log(num_nodes) + 1)
+
+        for move in moves:
+            if move is not None:
+                if tuple(move) not in explored:
+                    g_score = depth  # Using depth as g_score
+                    h_score = euclidean_distance(move, goal_state)
+                    f_score = g_score + h_score
+                    frontier.put((f_score, move))
+
+    return None
