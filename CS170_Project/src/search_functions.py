903d27189ec07bc2d9623c7413185b9c0aca8853
addyhana <addyhana@gmail.com>
2024-05-03 17:45:12 -0700

    ucs work !

--- CS170_Project/src/search_functions.py
+++ CS170_Project/src/search_functions.py
@@ -16,44 +16,72 @@ class Algorithms:
             if np.array_equal(target, array):
                 return True
         return False
-    
+
+    def state_to_tuple(state):
+        #converts array state to a hashable tuple for tracking parent/child relationship
+        return tuple(map(tuple, state))
     
     def ucs(self):
-        state = State()
+        state = State()  # assuming State is your state representation class
         frontier = PriorityQueue()
-        explored = []
-        path = []
-        depth = 0
+        explored = set()
+        parent_map = {}   # stores parent-child relationships for path reconstruction
+        depth_map = {}    # stores depths of each state
         order_determiner = 0
+        num_nodes = 0 
+        biggest_queue = 0 
         
-        frontier.put((depth, self.initial_state))
-        num_nodes = 1 # start at 1 to account for initial state
+        initial_state_tuple = Algorithms.state_to_tuple(self.initial_state)
+        goal_state_tuple = Algorithms.state_to_tuple(self.goal_state)
+        
+        frontier.put((0, initial_state_tuple))  # initial state with depth 0
+        depth_map[initial_state_tuple] = 0
         
         while not frontier.empty():
-            curr_state = frontier.get()[1]
-            path.append(curr_state)
+            if frontier.qsize() > biggest_queue: 
+                biggest_queue = frontier.qsize()
+            
+            curr_depth, curr_state_tuple = frontier.get()
+            curr_state = np.array(curr_state_tuple)
+            num_nodes += 1
             
             if np.array_equal(curr_state, self.goal_state):
-                #print("found ucs goal")
+                # reconstruct direct path using parent_map
+                path = []
+                while curr_state_tuple in parent_map:
+                    path.append(curr_state)
+                    curr_state_tuple = parent_map[curr_state_tuple]
+                    curr_state = np.array(curr_state_tuple)
+                path.append(self.initial_state)  # add the initial state to the path
+                path.reverse()  # reverse to get path from start to goal
+                print("Goal reached at depth:", len(path)-1 )
+                print("Most nodes in frontier at a single time:", biggest_queue )
+                print("Nodes expanded:", num_nodes)
                 return path
             
-            explored.append(curr_state)
+            explored.add(curr_state_tuple)
             state.set_state(state=curr_state)
             
-            up_move = state.move_up()
-            down_move = state.move_down()
-            left_move = state.move_left()
-            right_move = state.move_right()
-            moves = [up_move, down_move, left_move, right_move]
+            # all possible moves
+            moves = [
+                state.move_up(),
+                state.move_down(),
+                state.move_left(),
+                state.move_right()
+            ]
             
-            num_nodes += 1
-            depth = np.floor(np.log(num_nodes) + 1)
-            
-            for move in moves: 
+            for move in moves:
                 if move is not None:
-                    if not any(np.array_equal(move, item[1]) for item in frontier.queue):
-                        if not self.is_array_in_list(move, explored):
+                    move_tuple = Algorithms.state_to_tuple(move)
+                    if move_tuple not in explored:
+                        # calculate the new depth for the move
+                        new_depth = curr_depth + 1
+                        if move_tuple not in depth_map or new_depth < depth_map[move_tuple]:
+                            depth_map[move_tuple] = new_depth
+                            parent_map[move_tuple] = curr_state_tuple
                             order_determiner += 1
-                            frontier.put((depth + order_determiner, move))
-
+                            frontier.put((new_depth + order_determiner, move_tuple))
+        
+        print("Goal not found.")
+        print("Nodes expanded:", num_nodes)
         return None
--- CS170_Project/src/state.py
+++ CS170_Project/src/state.py
@@ -35,8 +35,8 @@ class State:
     def set_goal(self):
         return np.array([
             [1, 2, 3], 
-            [8, 0, 4], 
-            [7, 6, 5]
+            [4, 5, 6], 
+            [7, 8, 0]
         ])
     
     def set_state(self,
