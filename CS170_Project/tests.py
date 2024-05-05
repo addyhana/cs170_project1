b7e40dc1d59882cc33dc22ebd8be97f9d0e66679
addyhana <addyhana@gmail.com>
2024-05-05 13:26:33 -0700

    adding trace functionality for proj specs

--- CS170_Project/tests.py
+++ CS170_Project/tests.py
@@ -9,44 +9,55 @@ state = State()
 goal_state = state.set_goal()
 
 #------------------------------------------
+# trace case
+#------------------------------------------
+search = Algorithms(initial_state=state.set_start(start = np.array([[1, 0, 3], 
+                                            [4, 2, 6], 
+                                            [7, 5, 8]
+                                        ])),
+                    goal_state=goal_state)
+solution = search.eucDistTRACE()
+
+
+#------------------------------------------
 # all 6 test cases
 #------------------------------------------
-search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 3], 
-                                            [4, 5, 6], 
-                                            [7, 8, 0]
-                                        ])),
-                    goal_state=goal_state)
-print("TRIVIAL")
-solution = search.eucDist()
+# search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 3], 
+#                                             [4, 5, 6], 
+#                                             [7, 8, 0]
+#                                         ])),
+#                     goal_state=goal_state)
+# print("TRIVIAL")
+# solution = search.eucDist()
 
-search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 3], 
-                                            [4, 5, 6], 
-                                            [7, 0, 8]
-                                        ])),
-                    goal_state=goal_state)
-print("VERY EASY")
-solution = search.eucDist()
+# search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 3], 
+#                                             [4, 5, 6], 
+#                                             [7, 0, 8]
+#                                         ])),
+#                     goal_state=goal_state)
+# print("VERY EASY")
+# solution = search.eucDist()
 
-search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 0], 
-                                            [4, 5, 3], 
-                                            [7, 8, 6]
-                                        ])),
-                    goal_state=goal_state)
-print("EASY")
-solution = search.eucDist()
+# search = Algorithms(initial_state=state.set_start(start = np.array([[1, 2, 0], 
+#                                             [4, 5, 3], 
+#                                             [7, 8, 6]
+#                                         ])),
+#                     goal_state=goal_state)
+# print("EASY")
+# solution = search.eucDist()
 
-search = Algorithms(initial_state=state.set_start(start = np.array([[0, 1, 2], 
-                                            [4, 5, 3], 
-                                            [7, 8, 6]
-                                        ])),
-                    goal_state=goal_state)
-print("DOABLE")
-solution = search.eucDist()
+# search = Algorithms(initial_state=state.set_start(start = np.array([[0, 1, 2], 
+#                                             [4, 5, 3], 
+#                                             [7, 8, 6]
+#                                         ])),
+#                     goal_state=goal_state)
+# print("DOABLE")
+# solution = search.eucDist()
 
-search = Algorithms(initial_state=state.set_start(start = np.array([[8, 7, 1], 
-                                            [6, 0, 2], 
-                                            [5, 4, 3]
-                                        ])),
-                    goal_state=goal_state)
-print("OH BOY")
-solution = search.eucDist()
+# search = Algorithms(initial_state=state.set_start(start = np.array([[8, 7, 1], 
+#                                             [6, 0, 2], 
+#                                             [5, 4, 3]
+#                                         ])),
+#                     goal_state=goal_state)
+# print("OH BOY")
+# solution = search.eucDist()
